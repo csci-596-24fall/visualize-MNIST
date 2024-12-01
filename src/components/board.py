45461d2d2e1c183e9177b8c938c334fb1e050761
Erinyin627 <muyangyi@usc.edu>
2024-12-01 00:38:26 -0800

    Update board.py
    Add stroke width to 10

--- src/components/board.py
+++ src/components/board.py
@@ -6,7 +6,7 @@ from utils.constant import NN_SIZE, SCREEN_COLOR, SCREEN_DIVIDER
 
 class Draw:
     # TODO: adjust stroke width to be thicker
-    STROKE_WIDTH = 4
+    STROKE_WIDTH = 10
     def __init__(self, screen):
         self._color = WHITE
         self._screen = screen
